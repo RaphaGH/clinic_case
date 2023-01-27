8343455811404fb88c56055538f9514136e47680
RaphaGH <107068951+RaphaGH@users.noreply.github.com>
2023-01-27 17:44:22 -0500

    Test unitarios

---
+++ src/main/java/com/group/clinica/service/impl/Deuda_pagarServiceImplTest.java
@@ -0,0 +1,34 @@
+package com.group.clinica.service.impl;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Deuda_pagarServiceImplTest {
+
+    @org.junit.jupiter.api.Test
+    void comprobarDeudaPagada() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void all() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void newDeuda_pagar() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void one() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testComprobarDeudaPagada() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void replaceDeuda_pagar() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void deleteDeuda_pagar() {
+    }
+}
---
+++ src/main/java/com/group/clinica/service/impl/Horario_atencionServiceImplTest.java
@@ -0,0 +1,30 @@
+package com.group.clinica.service.impl;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Horario_atencionServiceImplTest {
+
+    @org.junit.jupiter.api.Test
+    void all() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void verificarHorarioDisponible() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void newHorario_atencion() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void one() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void replaceHorario_atencion() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void deleteHorario_atencion() {
+    }
+}
